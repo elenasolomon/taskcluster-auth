9c425484b7f41379dc5c4b782ab99dc78233dec3
Brian Stack <bstack@mozilla.com>
2017-02-03 11:54:23 -0800

    Add level to azure sas creds

--- src/azure.js
+++ src/azure.js
@@ -3,7 +3,7 @@ var api         = require('./v1');
 
 api.declare({
   method:     'get',
-  route:      '/azure/:account/table/:table/read-write',
+  route:      '/azure/:account/table/:table',
   name:       'azureTableSAS',
   input:      undefined,
   output:     'azure-table-access-response.json#',
@@ -13,24 +13,68 @@ api.declare({
   title:      "Get Shared-Access-Signature for Azure Table",
   description: [
     "Get a shared access signature (SAS) string for use with a specific Azure",
-    "Table Storage table.  Note, this will create the table, if it doesn't",
-    "already exist."
+    "Table Storage table. By not specifying a level as in azureTableSASLevel,",
+    "you always get read-write from this. Note: This will create the",
+    "table, if it doesn't already exist.",
   ].join('\n')
 }, async function(req, res) {
-  // Get parameters
   var account   = req.params.account;
   var tableName = req.params.table;
 
   // Check that the client is authorized to access given account and table
   if (!req.satisfies({
     account:    account,
-    table:      tableName
+    table:      tableName,
   })) {
     return;
   }
+  return await azureTableSASBase(res, this.azureAccounts, account, tableName);
+});
+
+api.declare({
+  method:     'get',
+  route:      '/azure/:account/table/:table/:level',
+  name:       'azureTableSASLevel',
+  input:      undefined,
+  output:     'azure-table-access-response.json#',
+  deferAuth:  true,
+  stability:  'stable',
+  scopes:     [['auth:azure-table-access:<account>/<table>/<level>']],
+  title:      "Get Shared-Access-Signature for Azure Table (with level)",
+  description: [
+    "Get a shared access signature (SAS) string for use with a specific Azure",
+    "Table Storage table.  This function is quite similar to azureTableSAS,",
+    "except that it takes a level as well. We need two separate functions because",
+    "the other function was defined first without the concept of a level.",
+    "Note: If level is 'read-write', this will create the",
+    "table, if it doesn't already exist.",
+  ].join('\n')
+}, async function(req, res) {
+  var account   = req.params.account;
+  var tableName = req.params.table;
+  var level     = req.params.level;
+
+  if (!['read-write', 'read-only'].includes(level)) {
+    return res.status(404).json({
+      message:    "Level '" + level + "' is not valid. Must be one of ['read-write', 'read-only']."
+    });
+  }
+
+  // Check that the client is authorized to access given account and table
+  if (!req.satisfies({
+    account:    account,
+    table:      tableName,
+    level:      level,
+  })) {
+    return;
+  }
+  return await azureTableSASBase(res, this.azureAccounts, account, tableName, level);
+});
+
+async function azureTableSASBase(res, azureAccounts, account, tableName, level='read-write') {
 
   // Check that the account exists
-  if (!this.azureAccounts[account]) {
+  if (!azureAccounts[account]) {
     return res.status(404).json({
       message:    "Account '" + account + "' not found, can't delegate access"
     });
@@ -39,17 +83,21 @@ api.declare({
   // Construct client
   var table = new azure.Table({
     accountId:  account,
-    accessKey:  this.azureAccounts[account]
+    accessKey:  azureAccounts[account]
   });
 
   // Create table ignore error, if it already exists
-  try {
-    await table.createTable(tableName);
-  } catch (err) {
-    if (err.code !== 'TableAlreadyExists') {
-      throw err;
+  if (level === 'read-write') {
+    try {
+      await table.createTable(tableName);
+    } catch (err) {
+      if (err.code !== 'TableAlreadyExists') {
+        throw err;
+      }
     }
   }
+
+  let perm = level === 'read-write';
 
   // Construct SAS
   var expiry = new Date(Date.now() + 25 * 60 * 1000);
@@ -58,9 +106,9 @@ api.declare({
     expiry:   expiry,
     permissions: {
       read:       true,
-      add:        true,
-      update:     true,
-      delete:     true
+      add:        perm,
+      update:     perm,
+      delete:     perm,
     }
   });
 
@@ -69,4 +117,4 @@ api.declare({
     sas:      sas,
     expiry:   expiry.toJSON()
   });
-});
+};
--- test/azure_test.js
+++ test/azure_test.js
@@ -5,6 +5,8 @@ suite('azure table (sas)', function() {
   var helper      = require('./helper');
   var slugid      = require('slugid');
   var _           = require('lodash');
+  var azure       = require('fast-azure-storage');
+  var taskcluster = require('taskcluster-client');
 
   test('azureTableSAS', function() {
     return helper.auth.azureTableSAS(
@@ -14,6 +16,60 @@ suite('azure table (sas)', function() {
       assert(typeof(result.sas) === 'string', "Expected some form of string");
       assert(new Date(result.expiry).getTime() > new Date().getTime(),
              "Expected expiry to be in the future");
+    });
+  });
+
+  test('azureTableSASLevel (read-write)', async function() {
+    let res = await helper.auth.azureTableSASLevel(
+      helper.testaccount,
+      'TestTable',
+      'read-write',
+    ).then(function(result) {
+      assert(typeof(result.sas) === 'string', "Expected some form of string");
+      assert(new Date(result.expiry).getTime() > new Date().getTime(),
+             "Expected expiry to be in the future");
+      return result;
+    });
+    let table = new azure.Table({
+      accountId: helper.testaccount,
+      sas: res.sas,
+    });
+    // This should not error since this is read-write
+    return table.insertEntity('TestTable', {PartitionKey: taskcluster.slugid(), RowKey: 'c'});
+  });
+
+  test('azureTableSASLevel (read-only)', async function() {
+    let res = await helper.auth.azureTableSASLevel(
+      helper.testaccount,
+      'TestTable',
+      'read-only',
+    ).then(function(result) {
+      assert(typeof(result.sas) === 'string', "Expected some form of string");
+      assert(new Date(result.expiry).getTime() > new Date().getTime(),
+             "Expected expiry to be in the future");
+      return result;
+    });
+    let table = new azure.Table({
+      accountId: helper.testaccount,
+      sas: res.sas,
+    });
+    // This should not error since this is read-write
+    return table.insertEntity('TestTable', {PartitionKey: taskcluster.slugid(), RowKey: 'c'}).then(() => {
+      assert(false, "This should not have been allowed to write!");
+    }, (err) => {
+      assert.equal(err.code, 'ResourceNotFound', 'This should not be able to see the table at all.');
+    });
+  });
+
+  test('azureTableSASLevel (invalid level)', function() {
+    return helper.auth.azureTableSASLevel(
+      helper.testaccount,
+      'TestTable',
+      'foo-bar-baz',
+    ).then(function(result) {
+      assert(false, "This should have thrown an error");
+    }).catch(function(err) {
+      assert.equal(err.message, "Level 'foo-bar-baz' is not valid. Must be one of ['read-write', 'read-only'].");
     });
   });
 
